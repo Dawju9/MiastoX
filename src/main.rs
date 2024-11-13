a3bda21814c2944706f09ae84a23bafac0fbad15
A ya Whitekali <dawju9@gmail.com>
2024-11-13 12:22:32 +0100

    rs
    a

--- src/main.rs
+++ src/main.rs
@@ -45,3 +45,8 @@ fn print_help() {
 fn print_version() {
     println!("MiastoX version 0.1.0");
 }
+// Use of a mod or pub mod is not actually necessary.
+pub mod built_info {
+    // The file has been placed there by the build script.
+    include!(concat!(env!("OUT_DIR"), "/built.rs"));
+ }
