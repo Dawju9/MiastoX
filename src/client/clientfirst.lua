04031f3963a631399e54a55c5e76ecaa3cbdf693
A ya Whitekali <dawju9@gmail.com>
2024-11-15 13:15:07 +0100

    "Updated sourcemap.json to add new children nodes and modify existing ones."

---
+++ src/client/clientfirst.lua
@@ -0,0 +1,10 @@
+-- @Stratiz 2022
+-- This is where your scripts on the client are initialized. You dont need to modify this file.
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local require = require(ReplicatedStorage:WaitForChild("SharedModules"):WaitForChild("Eden"))
+
+require:InitModules {
+	-- (Optional) Module Paths
+	DisplayInfo = shared("Shared/DisplayInfo")
+}
