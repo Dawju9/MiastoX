043582b61c9db84323caf7832e902063d1558036
A ya Whitekali <dawju9@gmail.com>
2024-11-15 13:16:20 +0100

    "Added FistCheck module and script to clientfirst.lua"

--- src/client/clientfirst.lua
+++ src/client/clientfirst.lua
@@ -1,10 +1,17 @@
--- @Stratiz 2022
--- This is where your scripts on the client are initialized. You dont need to modify this file.
-
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local require = require(ReplicatedStorage:WaitForChild("SharedModules"):WaitForChild("Eden"))
 
 require:InitModules {
-	-- (Optional) Module Paths
-	DisplayInfo = shared("Shared/DisplayInfo")
+    -- (Optional) Module Paths
+    DisplayInfo = shared("Shared/DisplayInfo"),
+    FistCheck = shared("Client/Scripts/FistCheck")
 }
+
+-- FistCheck Script
+local FistCheck = require("FistCheck")
+
+local function onFistCheck()
+    -- Code to check for fist
+end
+
+FistCheck:Init(onFistCheck)
